47b1d3fb907b4515123f37428c3cec1fdb3014d9
João Fernandes <joaovitorfernandes2@gmail.com>
2017-06-25 03:28:24 -0300

    Change data type to a struct that contains key: val

--- hash.c
+++ hash.c
@@ -2,32 +2,49 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TABLE_SIZE 100000
+#define TABLE_SIZE 10000
 #define WORD_SIZE 100
 
+struct item {
+  char * key;
+  int * value;
+};
 
 unsigned long hash(unsigned char *str) {
   unsigned long hash = 5381;
   int c;
 
   while (c = *str++)
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    hash = ((hash << 5) + hash) + c;
 
   return hash % TABLE_SIZE;
 }
 
+struct item *createItem(char *key, int value) {
+  struct item *newItem = (struct item *) malloc(sizeof(struct item *));
+  newItem->key = calloc(WORD_SIZE, sizeof(char));
+  newItem->value = malloc(sizeof(int));
+  
+  strcpy(newItem->key, key);
+  *newItem->value = value;
+  
+  return newItem;
+}
+
 int main(int argc, char **argv) {
-  char string[WORD_SIZE];
-  char ** hashtable;
-  hashtable = calloc(TABLE_SIZE, sizeof(char **));
-  for (int i = 0; i < TABLE_SIZE; i++) {
-    hashtable[i] = calloc(WORD_SIZE, sizeof(char *));
-    memset(hashtable[i], '\0', sizeof(char *) * WORD_SIZE);
+  struct item ** hashtable;
+  hashtable = (struct item **) calloc(TABLE_SIZE, sizeof(struct item **));
+
+  int keyId = 0;
+  char key[WORD_SIZE];
+  while (scanf(" %[^\n]s", key) == 1) {
+    unsigned long index = hash(key);
+    hashtable[index] = createItem(key, keyId++);
   }
 
-  while (scanf(" %[^\n]s", string) == 1) {
-    unsigned long index = hash(string);
-    strcpy(hashtable[index], string);
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    if (hashtable[i]->key && hashtable[i]->value)
+      printf("'%s': '%d'\n", hashtable[i]->key, *hashtable[i]->value);
   }
 
   return 0;
